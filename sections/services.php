408b32b4476a2b361eabd8a24f1aad128b9c7207
Brandon Espinoza <espinozab100@gmail.com>
2018-01-17 14:25:18 -0600

    Mobile layour for services is completed

--- sections/services.php
+++ sections/services.php
@@ -1,38 +1,83 @@
-<div class="col-md-12 services-row">
-  <div class="col-md-6 service-info vertical-align">
-    <div class="vertical-section">
-      <h1>Entrepreneur</h1>
-      <h1>Headline.</h1>
-      <p>Probo abdo olim roto utrum macto ventosus sed. Abigo et verto delenit ymo sit tamen ratis eligo. Pertineo cui imputo nisl singularis vel euismod odio. Augue epulae eros bis <a href="index.php">vero iriure abigo</a>.</p>
+<div id="desktop">
+  <div class="col-md-12 services-row">
+    <div class="col-sm-6 col-md-6 col-lg-6 service-info vertical-align">
+      <div class="vertical-section">
+        <h1>Entrepreneur</h1>
+        <h1>Headline.</h1>
+        <p>Probo abdo olim roto utrum macto ventosus sed. Abigo et verto delenit ymo sit tamen ratis eligo. Pertineo cui imputo nisl singularis vel euismod odio. Augue epulae eros bis <a href="index.php">vero iriure abigo</a>.</p>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-6 col-lg-6 service-one-bg">
+      <!--     bg image is on css/sections/services.css -->
     </div>
   </div>
-  <div class="col-md-6 service-one-bg">
-    <!--     bg image is on css/sections/services.css -->
+
+  <div class="col-md-12 services-row">
+    <div class="col-sm-6 col-md-6 col-lg-6 service-two-bg">
+      <!--     bg image is on css/sections/services.css -->
+    </div>
+    <div class="col-sm-6 col-md-6 col-lg-6 service-info vertical-align">
+      <div class="vertical-section">
+        <h1>Managers</h1>
+        <h1>Headline.</h1>
+        <p>Vero antehabeo facilisi eu consectetuer consectetuer nonummy caecus neque modo. Ut typicus luptatum indoles jus. Accumsan dignissim acsi imputo zelus. Consequat feugait usitas caecus molior modo hos multo minim dolor in appellatio. <a>Esse diam premo</a>.</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-12 services-row">
+    <div class="col-sm-6 col-md-6 col-lg-6 service-info vertical-align">
+      <div class="vertical-section">
+        <h1>Investor</h1>
+        <h1>Headline.</h1>
+        <p>Paratus aliquip velit nullus neo dolore at gravis sagaciter at jumentum. Quadrum neque usitas dolor quod. Enim cui interdico commodo <a href="index.php">dolore demoveo</a> fatua valde euismod saluto opes.</p>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-6 col-lg-6 service-three-bg">
+      <!--     bg image is on css/sections/services.css -->
+    </div>
   </div>
 </div>
 
-<div class="col-md-12 services-row">
-  <div class="col-md-6 service-two-bg">
-    <!--     bg image is on css/sections/services.css -->
-  </div>
-  <div class="col-md-6 service-info vertical-align">
-    <div class="vertical-section">
-      <h1>Managers</h1>
-      <h1>Headline.</h1>
-      <p>Vero antehabeo facilisi eu consectetuer consectetuer nonummy caecus neque modo. Ut typicus luptatum indoles jus. Accumsan dignissim acsi imputo zelus. Consequat feugait usitas caecus molior modo hos multo minim dolor in appellatio. <a>Esse diam premo</a>.</p>
-    </div>
-  </div>
-</div>
 
-<div class="col-md-12 services-row">
-  <div class="col-md-6 service-info vertical-align">
-    <div class="vertical-section">
-      <h1>Investor</h1>
-      <h1>Headline.</h1>
-      <p>Paratus aliquip velit nullus neo dolore at gravis sagaciter at jumentum. Quadrum neque usitas dolor quod. Enim cui interdico commodo <a href="index.php">dolore demoveo</a> fatua valde euismod saluto opes.</p>
+<!-- Mobile Layout -->
+<div id="mobile">
+  <div class="col-md-12 services-row">
+    <div class="col-sm-12 service-info vertical-align">
+      <div class="vertical-section">
+        <h1>Entrepreneur</h1>
+        <h1>Headline.</h1>
+        <p>Probo abdo olim roto utrum macto ventosus sed. Abigo et verto delenit ymo sit tamen ratis eligo. Pertineo cui imputo nisl singularis vel euismod odio. Augue epulae eros bis <a href="index.php">vero iriure abigo</a>.</p>
+      </div>
+    </div>
+    <div class="col-sm-12 service-one-bg">
+      <!--     bg image is on css/sections/services.css -->
     </div>
   </div>
-  <div class="col-md-6 service-three-bg">
-    <!--     bg image is on css/sections/services.css -->
+
+  <div class="col-md-12 services-row">
+    <div class="col-sm-12 service-info vertical-align">
+      <div class="vertical-section">
+        <h1>Managers</h1>
+        <h1>Headline.</h1>
+        <p>Vero antehabeo facilisi eu consectetuer consectetuer nonummy caecus neque modo. Ut typicus luptatum indoles jus. Accumsan dignissim acsi imputo zelus. Consequat feugait usitas caecus molior modo hos multo minim dolor in appellatio. <a>Esse diam premo</a>.</p>
+      </div>
+    </div>
+    <div class="col-sm-12 service-two-bg">
+      <!--     bg image is on css/sections/services.css -->
+    </div>
+  </div>
+
+  <div class="col-md-12 services-row">
+    <div class="col-sm-12 service-info vertical-align">
+      <div class="vertical-section">
+        <h1>Investor</h1>
+        <h1>Headline.</h1>
+        <p>Paratus aliquip velit nullus neo dolore at gravis sagaciter at jumentum. Quadrum neque usitas dolor quod. Enim cui interdico commodo <a href="index.php">dolore demoveo</a> fatua valde euismod saluto opes.</p>
+      </div>
+    </div>
+    <div class="col-sm-12 service-three-bg">
+      <!--     bg image is on css/sections/services.css -->
+    </div>
   </div>
 </div>
