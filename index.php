1f3f16fd9de244b79430fe847a463f4034ebe59f
Brandon Espinoza <espinozab100@gmail.com>
2018-01-17 00:30:55 -0600

    added bs css and js files. WIP top bar.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  	<title>DVL Code Test</title>
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="css/bootstrap-css/bootstrap.css">
+  </head>
+  <body>
+    <?php include('sections/header.php')?>
+  </body>
+  <script src="js/bootstrap-js/bootstrap.js"></script>
+</html>
