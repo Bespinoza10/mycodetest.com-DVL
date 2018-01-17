b05f04370fb16291109e514ecd10bee410071c79
Brandon Espinoza <espinozab100@gmail.com>
2018-01-17 16:10:57 -0600

    header and desktop-nav are modified for mobile

--- sections/header.php
+++ sections/header.php
@@ -1,17 +1,17 @@
 <div class="container-fluid">
-  <div class="col-md-1 center-column">
+  <div class="col-xs-6 col-sm-2 col-md-1 center-column">
     <img class="img-responsive top-logo" src="img/logo.svg">
   </div>
 </div>
 
 <header class="home-bg vertical-align">
-  <div class="col-md-8 center-column header-text">
+  <div class="col-sm-8 col-md-8 center-column header-text">
     <h2 class="header-sub-title center-text">BUSINESS PROCESS SOLUTIONS THAT</h2>
     <h1 class="header-title center-text">ACCELERATE YOUR GROWTH.</h1>
-    <div class="col-md-10 center-column header-text-two">
+    <div class="col-sm-10 col-md-10 center-column header-text-two">
       <p class="center-text header-sub-text">We will take care of your back office, so you can focus on amplifying your companies revenue, people and services.</p>
     </div>
-    <div class="col-md-4 header-btn-section center-column">
+    <div class="col-xs-8 col-sm-4 col-md-4 header-btn-section center-column">
       <button type="button" class="btn header-btn">WORK WITH US</button>
     </div>
 
