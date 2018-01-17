0e3715d56ab0fec6de7614bc6a4d83d7fcaad743
Brandon Espinoza <espinozab100@gmail.com>
2018-01-17 02:05:44 -0600

    Completed header section. Top bar section needs a couple tweaks, WIP.

--- sections/header.php
+++ sections/header.php
@@ -1,9 +1,21 @@
-<div class="row">
-  <div class="container-fluid">
-    <div class="col-md-1 center-column">
-      <img class="img-responsive top-logo" src="img/logo.svg">
-    </div>
+<div class="container-fluid">
+  <div class="col-md-1 center-column">
+    <img class="img-responsive top-logo" src="img/logo.svg">
   </div>
 </div>
+
+<header class="home-bg vertical-align">
+  <div class="col-md-8 center-column header-text">
+    <h2 class="header-sub-title center-text">BUSINESS PROCESS SOLUTIONS THAT</h2>
+    <h1 class="header-title center-text">ACCELERATE YOUR GROWTH.</h1>
+    <div class="col-md-10 center-column header-text-two">
+      <p class="center-text header-sub-text">We will take care of your back office, so you can focus on amplifying your companies revenue, people and services.</p>
+    </div>
+    <div class="col-md-4 header-btn-section center-column">
+      <button type="button" class="btn header-btn">WORK WITH US</button>
+    </div>
+
+  </div>
+</header>
 
 <h1>Hello World I am the Brwnmamba</h1>
