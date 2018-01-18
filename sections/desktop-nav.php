368b4193ad467053f76760189c5599e0d1010acf
Brandon Espinoza <espinozab100@gmail.com>
2018-01-18 02:10:59 -0600

    Styled according to style guide

--- sections/desktop-nav.php
+++ sections/desktop-nav.php
@@ -9,3 +9,6 @@
       <a>Contact</a>
   </div>
 </div>
+
+
+
--- sections/header.php
+++ sections/header.php
@@ -1,19 +1,86 @@
-<div class="container-fluid">
-  <div class="col-xs-6 col-sm-2 col-md-1 center-column">
-    <img class="img-responsive top-logo" src="img/logo.svg">
-  </div>
-</div>
+<nav class="navbar navbar-default" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
 
-<header class="home-bg vertical-align">
-  <div class="col-sm-8 col-md-8 center-column header-text">
-    <h2 class="header-sub-title center-text">BUSINESS PROCESS SOLUTIONS THAT</h2>
-    <h1 class="header-title center-text">ACCELERATE YOUR GROWTH.</h1>
-    <div class="col-sm-10 col-md-10 center-column header-text-two">
-      <p class="center-text header-sub-text">We will take care of your back office, so you can focus on amplifying your companies revenue, people and services.</p>
     </div>
-    <div class="col-xs-8 col-sm-4 col-md-4 header-btn-section center-column">
-      <button type="button" class="btn header-btn">WORK WITH US</button>
-    </div>
+    <a class="navbar-brand" href="#"><img class="nav-logo" src="img/logo.svg" /></a>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="navbar-collapse-1">
 
+      <ul class="nav navbar-nav navbar-left">
+        <li><a href="#">Menu</a></li>
+        <li><a href="#">Menu</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        <li><a href="#">Link</a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+</nav>
+
+
+
+
+<header class="home-bg">
+
+  <div class="bg-text-container col-sm-12 col-md-12 col-lg-12">
+    <div class="bg-text col-sm-12 col-md-10 col-lg-10 center-column">
+      <h2 class="bg-second-title center-text">BUSINESS PROCESS SOLUTIONS THAT</h2>
+      <h1 class="bg-title center-text">ACCELERATE YOUR GROWTH.</h1>
+      <div class="col-sm-10 col-md-8 col-lg-6 center-column bg-text-two">
+        <p class="center-text bg-sub-text">We will take care of your back office, so you can focus on amplifying your companies revenue, people and services.</p>
+      </div>
+      <div class="col-xs-10 col-sm-10 col-md-3 col-lg-4 center-column bg-btn-section">
+        <a class="btn bg-btn" href="#">WORK WITH US</a>
+      </div>
+    </div>
   </div>
+
+
+<!--
+  <div class="header-container col-sm-12 col-md-12 col-lg-12 vertical-align desktop-header">
+    <div class="col-sm-8 col-md-8 center-column header-text">
+      <h2 class="header-sub-title center-text">BUSINESS PROCESS SOLUTIONS THAT</h2>
+      <h1 class="header-title center-text">ACCELERATE YOUR GROWTH.</h1>
+      <div class="col-sm-10 col-md-10 center-column header-text-two">
+        <p class="center-text header-sub-text">We will take care of your back office, so you can focus on amplifying your companies revenue, people and services.</p>
+      </div>
+      <div class="col-xs-8 col-sm-4 col-md-4 header-btn-section center-column">
+        <button type="button" class="btn header-btn">WORK WITH US</button>
+      </div>
+    </div>
+  </div>
+-->
+
+<!--
+  <div class="header-container col-sm-12 col-md-12 col-lg-12 mobile-header">
+    <div class="col-sm-8 col-md-8 center-column mobile-header-text">
+      <h2 class="mobile-header-sub-title center-text">BUSINESS PROCESS SOLUTIONS THAT</h2>
+      <h1 class="mobile-header-title center-text">ACCELERATE YOUR GROWTH.</h1>
+      <div class="col-sm-10 col-md-10 center-column mobile-header-text-two">
+        <p class="center-text header-sub-text">We will take care of your back office, so you can focus on amplifying your companies revenue, people and services.</p>
+      </div>
+      <div class="col-xs-8 col-sm-4 col-md-4 mobile-header-btn-section center-column">
+        <button type="button" class="btn header-btn">WORK WITH US</button>
+      </div>
+    </div>
+  </div>
+-->
 </header>
